96c1967be4f619c086a8541de6ea821b3fc8f8e7
wtyang <912598129@qq.com>
2026-02-12 17:20:39 +0800

    refactor: 使 Vitest 配置兼容函数式 Vite 配置。

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,16 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/test/setup.ts',
-      css: true,
-    },
-  }),
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    typeof viteConfig === 'function' ? viteConfig(configEnv) : viteConfig,
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        css: true,
+      },
+    }),
+  ),
 );
